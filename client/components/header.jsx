5542249959dd41b7cfd9789c288716618eff142d
Stephanie B <sburcombe@gmail.com>
2019-07-04 22:28:01 +0000

    Completed User Can View The Average Grade Feature

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,16 @@ class Header extends React.Component {
 
   render() {
     return (
-      <header className="header">Student Grade Table</header>
+      <React.Fragment>
+        <div className="d-flex">
+          <h2 className="p-2">Student Grade Table</h2>
+          <h2 className= "p-2 ml-auto">Average Grade
+            <span className="badge badge-secondary">{this.props.gradeAverage}</span>
+          </h2>
+        </div>
+
+      </React.Fragment>
+
     );
   }
 }
