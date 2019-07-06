5ad34541dcf78649048e1ba99c1e19e3df2c1a07
Stephanie B <sburcombe@gmail.com>
2019-07-06 22:12:43 +0000

    Created User can add a grade feature

--- client/components/grade.jsx
+++ client/components/grade.jsx
@@ -5,9 +5,9 @@ class Grade extends React.Component {
   render(props) {
     return (
       <tr>
-        <td>{this.props.data.name} </td>
-        <td>{this.props.data.course}</td>
-        <td>{this.props.data.grade}</td>
+        <td >{this.props.data.name} </td>
+        <td >{this.props.data.course}</td>
+        <td >{this.props.data.grade}</td>
       </tr>
     );
   }
